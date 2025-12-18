decc4e415b389bb4dfdf616d62160ecc91240284
Shalama <shalamafadillaa@gmail.com>
2025-12-18 14:38:59 +0700

    Create a hash function
    Produce a hash code from compute a key as the input.

--- hash-map.js
+++ hash-map.js
@@ -4,4 +4,16 @@ class hashMap {
         this.capacity = 16;
         this.loadFactor = 0.75;
     }
+
+    // Create a hash map function to hash a key and produce a hash code
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+
+        return hashCode;
+    }
 }
