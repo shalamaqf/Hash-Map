4e21128672defb18e2333803613485971509304d
Shalama <shalamafadillaa@gmail.com>
2025-12-22 14:18:28 +0700

    Add codes for test the hash map

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 import { hashMap } from "./hash-map.js";
+
+const test = new hashMap();
+test.set('apple', 'red');
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+test.set('moon', 'silver')
+
+console.log(test.capacity);
+console.log(test.length())
+console.log(test.entries())
